ccfe7b4459f0433b4004da996bc9ad28354fa957
LeagueOfBugs <andres@Andress-MBP.lan>
2024-02-28 10:15:02 -0500

    Binary tree practice HC

---
+++ 226.js
@@ -0,0 +1,34 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function invertTree(root) {
+  if (root === null) {
+    return null;
+  }
+
+  // Swap left and right subtrees
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  // Recursively invert left and right subtrees
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
+
+const root1 = new TreeNode(1);
+root1.left = new TreeNode(2);
+root1.left.left = new TreeNode(4);
+root1.left.right = new TreeNode(7);
+root1.right = new TreeNode(3);
+root1.right.left = new TreeNode(6);
+root1.right.right = new TreeNode(9);
+
+console.log(invertTree(root1));
