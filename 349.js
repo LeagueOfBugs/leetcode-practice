cee8bbd4fd3dfbcffe5a6f3e4d5752de19a5b35b
LeagueOfBugs <andres@Andress-MBP.lan>
2024-03-10 17:33:29 -0400

    daily coding challenge

---
+++ 349.js
@@ -0,0 +1,19 @@
+const intersection = (nums1, nums2) => {
+   const set1 = new Set(nums1);
+   const set2 = new Set(nums2);
+
+   const result = [];
+
+   for (const num of set1) {
+     if (set2.has(num)) {
+       result.push(num);
+     }
+   }
+
+   return result;
+};
+
+const nums1 = [1, 2, 2, 1];
+const nums2 = [2, 2];
+
+console.log(intersection(nums1, nums2));
